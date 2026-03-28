8c35efe206c13becd50c62ab64d6b43c41669781
chuongtlgts01549-star <chuongtlgts01549@gmail.com>
2026-03-29 02:59:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -139,3 +139,75 @@ setInterval(()=>{
   videosDiv.innerHTML=""
   sidebarDiv.innerHTML=""
 },120000)
+// ... đoạn code cũ giữ nguyên ...
+
+// Render main feed
+function renderVideos(){
+  videosDiv.innerHTML=""
+  sidebarDiv.innerHTML=""
+  for(let v of videoList){
+    const box=document.createElement("div")
+    box.className="videoBox"
+
+    const thumb=document.createElement("video")
+    thumb.src=v.url
+    thumb.muted=true
+    thumb.onmouseover=()=>thumb.play()
+    thumb.onmouseout=()=>{thumb.pause();thumb.currentTime=0}
+    thumb.onclick=()=>{
+      player.src=v.url
+      player.play()
+      activeVideo=v
+      renderComments()
+    }
+
+    const title=document.createElement("p")
+    title.innerText=v.name
+
+    const view=document.createElement("p")
+    view.innerText="Views: "+v.views
+
+    setInterval(()=>{
+      v.views++
+      view.innerText="Views: "+v.views
+    },4000)
+
+    const like=document.createElement("button")
+    like.innerText="👍 "+v.likes
+    like.onclick=function(){v.likes++;like.innerText="👍 "+v.likes}
+
+    const dislike=document.createElement("button")
+    dislike.innerText="👎 "+v.dislikes
+    dislike.onclick=function(){v.dislikes++;dislike.innerText="👎 "+v.dislikes}
+
+    // ✅ Thêm nút Download
+    const downloadBtn = document.createElement("button")
+    downloadBtn.innerText = "⬇ Download"
+    downloadBtn.onclick = () => {
+      const a = document.createElement("a")
+      a.href = v.url
+      a.download = v.name
+      a.click()
+    }
+
+    box.appendChild(thumb)
+    box.appendChild(title)
+    box.appendChild(view)
+    box.appendChild(like)
+    box.appendChild(dislike)
+    box.appendChild(downloadBtn) // thêm vào đây
+    videosDiv.appendChild(box)
+
+    // Sidebar
+    const sideBox=document.createElement("div")
+    sideBox.className="videoBox"
+    sideBox.innerText=v.name
+    sideBox.onclick=()=>{
+      player.src=v.url
+      player.play()
+      activeVideo=v
+      renderComments()
+    }
+    sidebarDiv.appendChild(sideBox)
+  }
+}
